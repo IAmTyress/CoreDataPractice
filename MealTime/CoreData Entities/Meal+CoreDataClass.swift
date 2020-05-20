d2312706a35b5af80954978604d18cc4d6148f6c
Malysh Tim <malyshtim2000@yandex.ru>
2020-05-20 12:30:17 +0300

    Added 2 test CoreData projects

---
+++ MealTime/CoreData Entities/Meal+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Meal+CoreDataClass.swift
+//  MealTime
+//
+//  Created by Мас on 15.05.2020.
+//  Copyright © 2020 Ivan Akulov. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Meal)
+public class Meal: NSManagedObject {
+
+}
---
+++ MealTime/CoreData Entities/User+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  User+CoreDataClass.swift
+//  MealTime
+//
+//  Created by Мас on 15.05.2020.
+//  Copyright © 2020 Ivan Akulov. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(User)
+public class User: NSManagedObject {
+
+}
